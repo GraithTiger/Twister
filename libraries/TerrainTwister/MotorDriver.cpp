3def6fac89bf235a48832282ba3f0c0b8f5e87d2
GraithTiger <jcfisher@reality-overwritten.net>
2017-05-10 14:20:02 -0400

    finished, needs errors fixed

--- libraries/TerrainTwister/MotorDriver.cpp
+++ libraries/TerrainTwister/MotorDriver.cpp
@@ -1,9 +1,40 @@
 #include <MotorDriver.h>
 
-int MotorDriver::numC, MotorDriver::numCS;
-MotorDriver* MotorDriver::currentQueue;
-int* MotorDriver::analogQueue;
+int MotorDriver::numA = 0;
+int MotorDriver::numCC = 0;
+MotorDriver** MotorDriver::currentQueue;
+uint8_t* MotorDriver::analogQueue;
 volatile int* MotorDriver::analogReadings;
+volatile int MotorDriver::adcPos;
+
+void MotorDriver::init(int pinA, int pinB, int pinP){
+	A = pinA;
+	B = pinB;
+	P = pinP;
+	init();
+}
+
+
+//High Frequency Setup for Timer1 on Uno
+void MotorDriver::init(bool highFreq) {
+	#if defined(__AVR_ATmega168__)|| defined(__AVR_ATmega328P__)
+		if (highFreq && (P==9 || P==10)) {
+			// Timer 1 configuration
+			// prescaler: clockI/O / 1
+			// outputs enabled
+			// phase-correct PWM
+			// top of 400
+			//
+			// PWM frequency calculation
+			// 16MHz / 1 (prescaler) / 2 (phase-correct) / 400 (top) = 20kHz
+			TCCR1A = 0b10100000;
+			TCCR1B = 0b00010001;
+			ICR1 = 400;
+			HiFreq = true;
+		}
+	#endif
+	init();
+}
 
 void MotorDriver::init(){
 	if (A>=0) pinMode(A,OUTPUT);
@@ -12,31 +43,107 @@ void MotorDriver::init(){
 	if (CS>=0) {
 		if (CS>14) {
 			pinMode(CS, INPUT);
-			#if defined(__AVR_Atmega328P__)
-				CS -=14;
+			#if defined(__AVR_ATmega328P__)
+				CS -= 14;
 			#endif
 		}
+		initCurrent();
+	}
 	this->off();
 }
 
-void MotorDriver::init(int pinA, int pinB, int pinP){
-	A = pinA; 
-	B = pinB;
-	P = pinP;
-	init();
+void MotorDriver::initCurrent(){
+	aPos = queueAnalogSense(CS);
+	if (numCC==0) {
+		//setup timer2 interrupt here
+		TIFR2 &= B11111110; //
+		TIMSK2 |= B00000001; //set Timer 2 Interrupt enable
+		currentQueue = {&this};
+		numCC++;
+	}
+	else {
+		MotorDriver* newQueue[numCC+1];
+		for (int i = 0; i<numCC; i++) {
+			newQueue[i] = (*currentQueue)[i];
+		}
+		newQueue[numCC] = &this;
+		numCC++;
+		currentQueue = newQueue;
+	}
+}
+
+uint8_t MotorDriver::queueAnalogSense(uint8_t pin){
+	if (pin>14) {
+		pinMode(CS, INPUT);
+		#if defined(__AVR_ATmega328P__)
+			pin -= 14;
+		#endif
+	}
+	if (numA == 0) {
+		analogQueue = {pin};
+		analogReadings = {0};
+		adcPos = 0;
+		numA++;
+		enableADRI(pin);
+	}
+	else {
+		int newQueue[numA+1];
+		int newReadings[numA+1];
+		for (int i=0; i<numA; i++) {
+			newQueue[i] = (*analogQueue)[i];
+			newReadings[i] = (*analogReadings)[i];
+		}
+		newQueue[numA] = pin;
+		numA++;
+		analogQueue = newQueue;
+	}
+	return numA;
 }
 
 
 void MotorDriver::setSpeed(int speed){
+	CCenabled = false;
 	if (speed>0) forward(speed);
 	else if (speed<0) backward(-speed);
 	else off();
 }
 
+void MotorDriver::setCurrent(long mA){
+	Set = mA/34;
+	CCenabled = true;
+}
+
 void MotorDriver::brake(bool high /*= false*/){
-	digitalWrite(P,HIGH);
+	if (HiFreq && P==9) OCR1A = 400;
+	else if (HiFreq && P==10) OCR1B = 400;
+	else analogWrite(P,255);
 	digitalWrite(A,high);
 	digitalWrite(B,high);
 }
 
+//analog read loop
+ISR(ADC_vect) {
+	(*MotorDriver::analogReadings)[MotorDriver::adcPos] = ADCL | (ADCH << 8); //store reading
+	MotorDriver::adcPos++;
+	if (MotorDriver::adcPos>MotorDriver::numA) MotorDriver::adcPos=0;
+	ADMUX |= ((*MotorDriver::analogQueue)[1] & 0x07); //go to next pin to read
+	ADCSRA |=B01000000; // Set ADSC in ADCSRA (0x7A) to start the ADC conversion
+}
 
+//current control loop
+ISR(TIMER2_OVF_vect) {
+	for (int i = 0; i<MotorDriver::numCC; i++) {
+		MotorDriver* c = (*MotorDriver::currentQueue)[i];
+		if (c->CCenabled) {
+			if (c->Set==0)  c->off();
+			else if (c->Set>0) {
+				int val = (c->Kp) * ((c->Set) - MotorDriver::readAnalogSense(c->aPos));
+				c->forward(val);
+			}
+			else {
+				int val = (c->Kp) * ((c->Set) - MotorDriver::readAnalogSense(c->aPos));
+				c->backward(val);
+			}
+		}
+	}
+}
--- libraries/TerrainTwister/MotorDriver.h
+++ libraries/TerrainTwister/MotorDriver.h
@@ -11,42 +11,102 @@ class MotorDriver{
 	friend void ::TIMER2_OVF_vect(void);
 	
 	public:
-		MotorDriver(int pinA, int pinB, int pwmPin) : A(pinA), B(pinB), P(pwmPin), CS(-1){};
-		MotorDriver() : A(-1), B(-1), P(-1), CS(-1) {};
+		MotorDriver(int pinA, int pinB, int pwmPin) :
+			A(pinA), B(pinB), P(pwmPin), CS(-1), HiFreq(false),
+			aPos(-1), CCenabled(false), Set(0), Kp(10) {};
+		MotorDriver(int pinA, int pinB, int pwmPin, int CSpin) :
+			A(pinA), B(pinB), P(pwmPin), CS(CSpin), HiFreq(false),
+			aPos(-1), CCenabled(false), Set(0), Kp(10)  {};
+		MotorDriver() :
+			A(-1), B(-1), P(-1), CS(-1), HiFreq(false),
+			aPos(-1), CCenabled(false), Set(0), Kp(10)  {};
 		void init();
+		void init(bool);
 		void init(int pinA, int pinB, int pinP );
-		inline void MotorDriver::forward(int speed) {
-			analogWrite(P,speed);
-			digitalWrite(A,HIGH);
-			digitalWrite(B,LOW);
-		};
-
-		inline void backward(int speed){
-			analogWrite(P,speed);
-			digitalWrite(B,HIGH);
-			digitalWrite(A,LOW);
-		};
-		inline void off(){
-			digitalWrite(P,LOW);
-			digitalWrite(A,LOW);
-			digitalWrite(B,LOW);
-		}
 		void setSpeed(int);
 		void setCurrent(long);
 		void brake(bool high = false);
+		inline int getKp() { return Kp;}
+		inline void setKp( int k ) {Kp = k;}
 		static uint8_t queueAnalogSense(uint8_t);
 		static inline int readAnalogSense(uint8_t position) {
 			return *(analogQueue+position);
 		}
 	private:
-		int A, B, P, CS, Set;
+		int A, B, P, CS, Set, Kp;
 		uint8_t aPos;
-		bool CSenabled;
-		static int numC, numCS;
-		static MotorDriver *currentQueue;
-		static int* analogQueue;
+		bool CCenabled, HiFreq;
+		static int numA, numCC;
+		static MotorDriver** currentQueue;
+		static uint8_t* analogQueue;
+		static volatile int adcPos;
 		static volatile int* analogReadings;
-		static void queueCurrent();
+		void initCurrent();
+		inline void MotorDriver::forward(int speed) {
+			speed = constrain(speed,0,400);
+			if (HiFreq && P==9) OCR1A = speed;
+			else if (HiFreq && P==10) OCR1B = speed;
+			else analogWrite(P,speed*51/80);
+			digitalWrite(A,HIGH);
+			digitalWrite(B,LOW);
+		};
+
+		inline void backward(int speed){
+			speed = constrain(speed,0,400);
+			if (HiFreq && P==9) OCR1A = speed;
+			else if (HiFreq && P==10) OCR1B = speed;
+			else analogWrite(P,speed*51/80);
+			digitalWrite(B,HIGH);
+			digitalWrite(A,LOW);
+		};
+		inline void off(){
+			if (HiFreq && P==9) OCR1A = 0;
+			else if (HiFreq && P==10) OCR1B = 0;
+			else analogWrite(P,0);
+			digitalWrite(A,LOW);
+			digitalWrite(B,LOW);
+		}
+		static inline void enableADRI(uint8_t pin)
+		{
+			if (pin>14) {
+				#if defined(__AVR_ATmega328P__)
+					pin -= 14;
+				#endif
+			}
+			// clear ADLAR in ADMUX (0x7C) to right-adjust the result
+			// ADCL will contain lower 8 bits, ADCH upper 2 (in last two bits)
+			ADMUX &= B11011111;
+			// Set REFS1..0 in ADMUX (0x7C) to change reference voltage to the
+			// proper source (01)
+			ADMUX |= B01000000;
+			// Clear MUX3..0 in ADMUX (0x7C) in preparation for setting the analog
+			// input
+			ADMUX &= B11110000;
+			// Set MUX3..0 in ADMUX (0x7C) to read from AD8 (Internal temp)
+			// Do not set above 15! You will overrun other parts of ADMUX. A full
+			// list of possible inputs is available in Table 24-4 of the ATMega328
+			// datasheet
+			ADMUX |= (pin & 0x07);
+			// ADMUX |= B00001000; // Binary equivalent
+			// Set ADEN in ADCSRA (0x7A) to enable the ADC.
+			// Note, this instruction takes 12 ADC clocks to execute
+			ADCSRA |= B10000000;
+			// Clear ADATE in ADCSRA (0x7A) to disable auto-triggering.
+			ADCSRA &= ~B00100000;
+			// Clear ADTS2..0 in ADCSRB (0x7B) to set trigger mode to free running.
+			// This means that as soon as an ADC has finished, the next will be
+			// immediately started.
+			//ADCSRB &= B11111000;
+			// Set the Prescaler to 128 (16000KHz/128 = 125KHz)
+			// Above 200KHz 10-bit results are not reliable.
+			ADCSRA |= B00000111;
+			// Set ADIE in ADCSRA (0x7A) to enable the ADC interrupt.
+			// Without this, the internal interrupt will not trigger.
+			ADCSRA |= B00001000;
+			// Kick off the first ADC
+			// Set ADSC in ADCSRA (0x7A) to start the ADC conversion
+			ADCSRA |=B01000000;
+		}
 };
 
 #endif
